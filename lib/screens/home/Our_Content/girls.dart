87d5fd003c0045a0091de4d9016a73b794745810
Adam Bolton <adambolton13@gmail.com>
2021-03-09 17:07:47 -0500

    Our content now has Boys / Girl Tab to switch

---
+++ lib/screens/home/Our_Content/girls.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:the_final_word/components/loading.dart';
+import '../../../services/database.dart';
+import 'package:provider/provider.dart';
+import '../../../models/user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class GirlsOurContent extends StatefulWidget {
+  @override
+  _GirlsOurContentState createState() => _GirlsOurContentState();
+}
+
+class _GirlsOurContentState extends State<GirlsOurContent> {
+  final formKey = GlobalKey<FormState>();
+  final CollectionReference userProfile =
+      FirebaseFirestore.instance.collection('profiles');
+  String code;
+  Profile profile = Profile();
+  Profile friendProfile = Profile();
+
+  void addConnectionID(Profile profile, String uid) async {
+    return userProfile
+        .doc(profile.uid)
+        .update({'connected_to': uid})
+        .then((value) => print('Code Added to profile'))
+        .catchError((onError) => print('Failed to add'));
+  }
+
+  Future<void> loadCode(String code, Profile profile) async {
+    final CollectionReference codesCollection =
+        FirebaseFirestore.instance.collection('user_codes');
+
+    codesCollection.doc(code).get().then((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+        addConnectionID(profile, documentSnapshot.data()['user_id']);
+      }
+    }).catchError((onError) => print("Code not found / problem"));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<NewUser>(context);
+    return StreamBuilder<Profile>(
+      //load actual profile off user
+      stream: DatabaseService(uid: user.uid).profile,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          Profile profile = snapshot.data;
+          // check if a connection exists
+          if (profile.connected_to != "") {
+            // load connection profile
+            return StreamBuilder<Profile>(
+                stream: DatabaseService(uid: profile.connected_to).profile,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    Profile friendProfile = snapshot.data;
+                    // Create a new list of shared names
+                    List<String> nameSets = [];
+                    profile.girls_names.forEach((element) {
+                      if (friendProfile.girls_names.contains(element)) {
+                        nameSets.add(element);
+                      }
+                    });
+
+                    // Do the reserve of an connected to add
+                    if (friendProfile.connected_to != profile.uid) {
+                      addConnectionID(friendProfile, profile.uid);
+                    }
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                            'You are connected with ${friendProfile.first_name} ${friendProfile.last_name}'),
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount: nameSets.length,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                title: Text('${nameSets[index]}'),
+                              );
+                            },
+                          ),
+                        )
+                      ],
+                    );
+                  } else {
+                    return Loading();
+                  }
+                });
+          } else {
+            return Center(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextFormField(
+                          autofocus: true,
+                          decoration: InputDecoration(
+                            labelText: 'Title',
+                            border: OutlineInputBorder(),
+                          ),
+                          onSaved: (value) {
+                            code = value;
+                          },
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter a code to search';
+                            } else {
+                              return null;
+                            }
+                          }),
+                      RaisedButton(
+                          onPressed: () async {
+                            formKey.currentState.save();
+                            print(profile.uid);
+                            loadCode(code, profile);
+                          },
+                          child: Text('Search Code'))
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }
+        } else {
+          return Loading();
+        }
+      },
+    );
+  }
+}
--- lib/screens/home/our_content.dart
+++ lib/screens/home/our_content.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:the_final_word/components/loading.dart';
+import 'package:the_final_word/screens/home/Our_Content/boys.dart';
+import '../home/Our_Content/girls.dart';
 import '../../services/database.dart';
 import 'package:provider/provider.dart';
 import '../../models/user.dart';
@@ -14,7 +16,6 @@ class OurContent extends StatefulWidget {
 
 class _OurContentState extends State<OurContent> {
   final formKey = GlobalKey<FormState>();
-  bool _view;
   final CollectionReference userProfile =
       FirebaseFirestore.instance.collection('profiles');
   String code;
@@ -23,135 +24,26 @@ class _OurContentState extends State<OurContent> {
 
   void initState() {
     super.initState();
-    initView();
   }
 
-  void initView() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _view = prefs.getBool('view') ?? true;
-    });
-    print(_view);
-  }
+  static const tabs = [
+    Tab(icon: Icon(Icons.account_box_outlined)),
+    Tab(icon: Icon(Icons.article_outlined))
+  ];
 
-  void addConnectionID(Profile profile, String uid) async {
-    return userProfile
-        .doc(profile.uid)
-        .update({'connected_to': uid})
-        .then((value) => print('Code Added to profile'))
-        .catchError((onError) => print('Failed to add'));
-  }
-
-  Future<void> loadCode(String code, Profile profile) async {
-    final CollectionReference codesCollection =
-        FirebaseFirestore.instance.collection('user_codes');
-
-    codesCollection.doc(code).get().then((DocumentSnapshot documentSnapshot) {
-      if (documentSnapshot.exists) {
-        addConnectionID(profile, documentSnapshot.data()['user_id']);
-      }
-    }).catchError((onError) => print("Code not found / problem"));
-  }
+  final screens = [BoysOurContent(), GirlsOurContent()];
 
   @override
   Widget build(BuildContext context) {
-    final user = Provider.of<NewUser>(context);
-    return StreamBuilder<Profile>(
-      //load actual profile off user
-      stream: DatabaseService(uid: user.uid).profile,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          Profile profile = snapshot.data;
-          // check if a connection exists
-          if (profile.connected_to != "") {
-            // load connection profile
-            return StreamBuilder<Profile>(
-                stream: DatabaseService(uid: profile.connected_to).profile,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    Profile friendProfile = snapshot.data;
-                    // Create a new list of shared names
-                    List<String> nameSets = [];
-                    if (_view) {
-                      profile.girls_names.forEach((element) {
-                        if (friendProfile.girls_names.contains(element)) {
-                          nameSets.add(element);
-                        }
-                      });
-                    } else {
-                      profile.boys_names.forEach((element) {
-                        if (friendProfile.boys_names.contains(element)) {
-                          nameSets.add(element);
-                        }
-                      });
-                    }
-                    // Do the reserve of an connected to add
-                    if (friendProfile.connected_to != profile.uid) {
-                      addConnectionID(friendProfile, profile.uid);
-                    }
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                            'You are connected with ${friendProfile.first_name} ${friendProfile.last_name}'),
-                        Expanded(
-                          child: ListView.builder(
-                            itemCount: nameSets.length,
-                            itemBuilder: (context, index) {
-                              return ListTile(
-                                title: Text('${nameSets[index]}'),
-                              );
-                            },
-                          ),
-                        )
-                      ],
-                    );
-                  } else {
-                    return Loading();
-                  }
-                });
-          } else {
-            return Center(
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: Form(
-                  key: formKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      TextFormField(
-                          autofocus: true,
-                          decoration: InputDecoration(
-                            labelText: 'Title',
-                            border: OutlineInputBorder(),
-                          ),
-                          onSaved: (value) {
-                            code = value;
-                          },
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Please enter a code to search';
-                            } else {
-                              return null;
-                            }
-                          }),
-                      RaisedButton(
-                          onPressed: () async {
-                            formKey.currentState.save();
-                            print(profile.uid);
-                            loadCode(code, profile);
-                          },
-                          child: Text('Search Code'))
-                    ],
-                  ),
-                ),
-              ),
-            );
-          }
-        } else {
-          return Loading();
-        }
-      },
-    );
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              flexibleSpace: TabBar(tabs: tabs),
+            ),
+            body: TabBarView(
+              children: screens,
+            )));
   }
 }
